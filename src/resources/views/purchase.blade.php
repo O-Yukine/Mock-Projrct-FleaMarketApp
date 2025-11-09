e80cad3eabe5c970505346a53cad052cf57d35bc
Yukine <yukineriko@gmail.com>
2025-11-09 19:35:36 +0900

    商品購入前ページのスタイルとレスポンシブ対応

--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -13,29 +13,39 @@
                         <img src="{{ asset('storage/product_images/' . $product->product_image) }}" alt="商品画像">
                     </div>
                     <div class="order-information">
-                        <h4>{{ $product->name }}</h4>
+                        <h3>{{ $product->name }}</h3>
                         <p>¥{{ $product->price }}</p>
                     </div>
                 </div>
-                <div class="payment">
-                    <p>支払い方法</p>
-                    <select id="payment-method" class="paying-option" name="payment_method">
-                        <option value="">支払い方法を選択</option>
-                        <option value="convenience">コンビニ払い</option>
-                        <option value="credit">カード支払い</option>
-                    </select>
+                <div class="payment-type">
+                    <div class="payment-type__title">
+                        <h4>支払い方法</h4>
+                    </div>
+                    <div class="payment-type__contents">
+                        <select id="payment-method" class="paying-option" name="payment_method">
+                            <option value="">支払い方法を選択</option>
+                            <option value="convenience">コンビニ払い</option>
+                            <option value="credit">カード支払い</option>
+                        </select>
+                    </div>
                 </div>
+
                 <div class="shipping-address">
-                    <span>配送先</span><span><a href="/purchase/address/{{ $product->id }}">変更する</a></span>
-                    <input type="hidden" name="post_code"
-                        value="{{ $shipping_address['post_code'] ?? $user->profile->post_code }}">
-                    {{ $shipping_address['post_code'] ?? $user->profile->post_code }}
-                    <input type="hidden" name="address"
-                        value="{{ $shipping_address['address'] ?? $user->profile->address }}">
-                    {{ $shipping_address['address'] ?? $user->profile->address }}
-                    <input type="hidden" name="building"
-                        value="{{ $shipping_address['building'] ?? ($user->profile->building ?? '') }}">
-                    {{ $shipping_address['building'] ?? ($user->profile->building ?? '') }}
+                    <div class="shipping-address__title">
+                        <h4>配送先</h4>
+                        <a href="/purchase/address/{{ $product->id }}">変更する</a>
+                    </div>
+                    <div class="shipping-address__contents">
+                        <p>〒<input type="hidden" name="post_code"
+                                value="{{ $shipping_address['post_code'] ?? $user->profile->post_code }}">
+                            {{ $shipping_address['post_code'] ?? $user->profile->post_code }}</p>
+                        <span><input type="hidden" name="address"
+                                value="{{ $shipping_address['address'] ?? $user->profile->address }}">
+                            {{ $shipping_address['address'] ?? $user->profile->address }}</span>
+                        <span><input type="hidden" name="building"
+                                value="{{ $shipping_address['building'] ?? ($user->profile->building ?? '') }}">
+                            {{ $shipping_address['building'] ?? ($user->profile->building ?? '') }}</span>
+                    </div>
                 </div>
             </div>
             <div class="right-content">
@@ -49,8 +59,10 @@
                         <td id="payment-info"></td>
                     </tr>
                 </table>
-                <button class="order-complete__button">
-                    購入する</button>
+                <div class="order__submit">
+                    <button class="order__submit-button">
+                        購入する</button>
+                </div>
         </form>
     </div>
     </div>
