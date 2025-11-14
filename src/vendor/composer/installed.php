5031aee5747fc9697fc147cb7c22389a340193f8
Yukine <yukineriko@gmail.com>
2025-11-14 16:45:32 +0900

    stripeのインストール

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -1025,6 +1025,15 @@
             'aliases' => array(),
             'dev_requirement' => true,
         ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v18.2.0',
+            'version' => '18.2.0.0',
+            'reference' => '0acd7bdac84ad0f940d5da30c417170ce59b4fbc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'swiftmailer/swiftmailer' => array(
             'pretty_version' => 'v6.3.0',
             'version' => '6.3.0.0',
