69be06c4f5763e85e2442e5c1f65a6b3ac7e68ac
Yukine <yukineriko@gmail.com>
2025-11-15 18:55:27 +0900

    メール認証誘導画面のスタイルとレスポンシブ対応

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,19 +1,40 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('css')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Flea Market</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/verify-email.css') }}">
-@endsection
-@section('content')
-    <div class="verification">
-        <p>登録していただいたメールアドレスに認証メールを送付しました。</p>
-        <br>
-        <p>メール認証を完了してください</p>
+</head>
 
-        <a href="{{ $verificationUrl }}">認証はこちらから</a>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+                <a class="header__logo" href="/"><img src="{{ asset('images/COACHTECH.png') }}"
+                        alt="coachtech logo"></a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="email-verification">
+            <div class="contents">
+                <p>登録していただいたメールアドレスに認証メールを送付しました。</p>
+                <p>メール認証を完了してください</p>
+                <div class="verify-link">
+                    <a href="{{ $verificationUrl }}">認証はこちらから</a>
+                </div>
+                <form class="form" method="POST" action="{{ route('verification.send') }}">
+                    @csrf
+                    <button class="verification-resend__button">認証メールを再送する</button>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
 
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
-            <button>認証メールを再送する</button>
-        </form>
-    </div>
-@endsection
+</html>
